de7e706cf548f25825644f74d84d52058acf176b
yamami mohamed monsif <mohammedyamami05@gmail.com>
2024-04-12 13:46:54 +0100

    stored the needed html elements in variables

---
+++ main.js
@@ -0,0 +1,15 @@
+var day = document.getElementById('day');
+var month = document.getElementById('month');
+var year = document.getElementById('year');
+
+var labels = document.getElementsByTagName('label');
+
+var errorMessageDay = document.getElementById('errorMessageDay');
+var errorMessageMonth = document.getElementById('errorMessageMonth');
+var errorMessageYear = document.getElementById('errorMessageYear');
+
+var resultDay = document.getElementById('result-day');
+var resultMonth = document.getElementById('result-month');
+var resultYear = document.getElementById('result-year');
+
+var submitButton = document.getElementById('submitButton');
