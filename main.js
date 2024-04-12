f0f1c5c206123d1704c07b44af885afa42a9c82a
yamami mohamed monsif <mohammedyamami05@gmail.com>
2024-04-12 13:49:36 +0100

    validate the day number

--- main.js
+++ main.js
@@ -13,3 +13,21 @@ var resultMonth = document.getElementById('result-month');
 var resultYear = document.getElementById('result-year');
 
 var submitButton = document.getElementById('submitButton');
+
+
+// validate the day
+day.addEventListener('input', validateDay);
+function validateDay(monthInput) {
+    if(day.value != '') {
+        day.classList.remove('error');
+        labels[0].classList.remove('error');
+        errorMessageDay.textContent = '';
+    }
+
+    if(day.value > 31 || day.value < 1) {
+        day.classList.add('error');
+        labels[0].classList.add('error');
+        errorMessageDay.textContent = 'Must be a valid day';
+        return false;
+    }
+}
